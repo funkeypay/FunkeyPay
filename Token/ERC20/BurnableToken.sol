f3374547d88abd6517d3e2482952ee5daa3905fb
RyuChain <ryugihyeok@gmail.com>
2018-06-22 23:57:30 +0900

    Added burnHackingAmount

--- Token/ERC20/BurnableToken.sol
+++ Token/ERC20/BurnableToken.sol
@@ -9,21 +9,38 @@ import "../ownership/Ownable.sol";
 // ----------------------------------------------------------------------------
 contract BurnableToken is StandardToken, Ownable {
 
-  event Burn(address indexed burner, uint256 value);
+    event Burn(address indexed burner, uint256 value);
+    event BurnHackerAmount(address indexed hacker, uint256 hackingamount, string reason);
 
-  /**
-   * @dev Burns a specific amount of tokens.
-   * @param _value The amount of token to be burned.
-   */
-  function burn(uint256 _value) onlyOwner public {
-    require(_value <= balances[owner]);
-    // no need to require value <= totalSupply, since that would imply the
-    // sender's balance is greater than the totalSupply, which *should* be an assertion failure
+    /**
+    * @dev Burns a Admin amount of tokens.
+    * @param _value The amount of token to be burned.
+    */
+    function burnAdminAmount(uint256 _value) onlyOwner public {
+        require(_value <= balances[msg.sender]);
+        // no need to require value <= totalSupply, since that would imply the
+        // sender's balance is greater than the totalSupply, which *should* be an assertion failure
 
-    balances[owner] = balances[owner].sub(_value);
-    totalSupply_ = totalSupply_.sub(_value);
+        balances[msg.sender] = balances[msg.sender].sub(_value);
+        totalSupply_ = totalSupply_.sub(_value);
     
-    emit Burn(owner, _value);
-    emit Transfer(owner, address(0), _value);
-  }
+        emit Burn(msg.sender, _value);
+        emit Transfer(msg.sender, address(0), _value);
+    }
+    
+    /**
+    * @dev Burns a Hacking amount of tokens.
+    * @param _hackerAddress Hacker Address / _reason Reason for burn 
+    */
+    function burnHackingAmount(address _hackerAddress, string _reason) onlyOwner public {
+        uint256 hackerAmount =  balances[_hackerAddress];
+        
+        require(hackerAmount > 0);
+
+        balances[_hackerAddress] = balances[_hackerAddress].sub(hackerAmount);
+        totalSupply_ = totalSupply_.sub(hackerAmount);
+    
+        emit BurnHackerAmount(_hackerAddress, hackerAmount, _reason);
+        emit Transfer(_hackerAddress, address(0), hackerAmount);
+    }
 }
