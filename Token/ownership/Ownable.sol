43387400ef61900f5370659e305d49522b0675d5
RyuChain <ryugihyeok@gmail.com>
2018-06-23 00:01:07 +0900

    Added Operator

--- Token/ownership/Ownable.sol
+++ Token/ownership/Ownable.sol
@@ -6,43 +6,41 @@ pragma solidity ^0.4.24;
 // functions, this simplifies the implementation of "user permissions".
 // ----------------------------------------------------------------------------
 contract Ownable {
-  address public owner;
+    address public owner;
+    address public operator;
 
-  event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+    event OperatorTransferred(address indexed previousOperator, address indexed newOperator);
 
-  /**
-   * @dev The Ownable constructor sets the original `owner` of the contract to the sender
-   * account.
-   */
-  constructor() public {
-    owner = msg.sender;
-  }
+    /**
+    * @dev The Ownable constructor sets the original `owner` of the contract to the sender
+    * account.
+    */
+    constructor() public {
+        owner = msg.sender;
+        operator = msg.sender;
+    }
 
-  /**
-   * @dev Throws if called by any account other than the owner.
-   */
-  modifier onlyOwner() {
-    require(msg.sender == owner);
-    _;
-  }
+    /**
+    * @dev Throws if called by any account other than the owner & operator
+    */
 
-  /**
-   * @dev Allows the current owner to transfer control of the contract to a newOwner.
-   * @param _newOwner The address to transfer ownership to.
-   */
-  function transferOwnership(address _newOwner) public onlyOwner {
-    _transferOwnership(_newOwner);
-  }
+    modifier onlyOwner() { require(msg.sender == owner); _; }
+    modifier onlyOwnerOrOperator() { require(msg.sender == owner || msg.sender == operator); _; }
 
-  /**
-   * @dev Transfers control of the contract to a newOwner.
-   * @param _newOwner The address to transfer ownership to.
-   */
-  function _transferOwnership(address _newOwner) internal {
-    require(_newOwner != address(0));
-    
-    emit OwnershipTransferred(owner, _newOwner);
-    
-    owner = _newOwner;
-  }
+    /**
+    * @dev Allows the current owner to transfer control of the contract to a newOwner.
+    * @param _newOwner The address to transfer ownership to.
+    */
+    function transferOwnership(address _newOwner) public onlyOwner {
+        require(_newOwner != address(0));
+        owner = _newOwner;
+        emit OwnershipTransferred(owner, _newOwner);
+    }
+  
+    function transferOperator(address _newOperator) public onlyOwner {
+        require(_newOperator != address(0));
+        operator = _newOperator;
+        emit OperatorTransferred(operator, _newOperator);
+    }
 }
